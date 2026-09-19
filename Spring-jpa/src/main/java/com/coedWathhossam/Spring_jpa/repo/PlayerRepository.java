56767576b2bd85998153335f946cec8598d77f2d
Hossam <your_email@example.com>
2026-09-19 18:18:43 +0300

    Delete Spring-jpa folder

--- Spring-jpa/src/main/java/com/coedWathhossam/Spring_jpa/repo/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coedWathhossam.Spring_jpa.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.coedWathhossam.Spring_jpa.Model.Player;
-
-public interface PlayerRepository extends JpaRepository<Player,Long> {
-
-}
